aaea1d620de7c0750e013440f05256a761704394
VadimCurca <vadim.curca14@gmail.com>
2021-04-12 16:05:45 +0300

    CLAS-15 Add Nitrite Java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,11 @@
-module hellofx {
+module ClassicGames {
     requires javafx.controls;
     requires javafx.fxml;
+    requires nitrite;
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.databind;
 
     opens org.ClassicGames to javafx.fxml;
+    opens org.ClassicGames.model to com.fasterxml.jackson.databind;
     exports org.ClassicGames;
 }
